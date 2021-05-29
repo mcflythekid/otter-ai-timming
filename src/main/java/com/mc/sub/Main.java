5bc28843e5ee0655cbfe89dd2fef4be8f155e0f8
mc <odopoc@gmail.com>
2021-05-30 04:35:06 +0700

    - update batch

--- src/main/java/com/mc/sub/Main.java
+++ src/main/java/com/mc/sub/Main.java
@@ -3,11 +3,32 @@ package com.mc.sub;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String file="What Are Shit Tests";
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String[] list = new String[]{
+                "Anal Sex",
+                "Control Your Woman's Self Esteem",
+                "CRP Patreon Exclusive- Women Are Like Dogs",
+                "Hookers and Escorts",
+                "Human Nature Is Real",
+                "Keep Your Insights To Yourself",
+                "Never Take A Girl Back",
+                "The Importance of Fucking a Girl Good",
+                "What Are Shit Tests"
+        };
+
+        for (String file : list) {
+            Converter.submit(
+                    "C:/Users/nha/Desktop/tmp/otter.ai/" + file + ".srt",
+                    "C:/Users/nha/Desktop/tmp/pre-process/" + file + ".srt",
+                    150);
+            Thread.sleep(500);
+        }
+    }
+
+    public static void main2(String[] args) throws IOException {
         Converter.submit(
-                "C:/Users/nha/Desktop/tmp/otter.ai/" + file + ".srt",
-                "C:/Users/nha/Desktop/tmp/pre-process/" + file + ".srt",
+                "test.srt",
+                "test-out.srt",
                 150);
     }
 }
